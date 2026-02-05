7f882f00f4b3ca6cb1743bc90e38983fd1a789ae
Priyanshi004 <sharmapriyanshi19799@gmail.com>
2026-02-05 11:53:14 +0530

    Start

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Premium Landing Page</h1>
+      <p>Welcome to your new website.</p>
+    </div>
+  )
+}
+
+export default App
